c641c98e49d89f3d50e2a3cf25997e21b76e3a6f
Atte Tuomisto <at.tuomisto@gmail.com>
2017-02-16 12:11:34 +0200

    First commit, working bot. Needs some improvements of course.

---
+++ mybot.js
@@ -0,0 +1,129 @@
+var Discord = require("discord.js");
+var request = require("request");
+var bot = new Discord.Client();
+var fs = require('fs');
+
+
+bot.on("message", msg => {
+  // Set the prefix
+  let prefix = "!";
+  // Exit and stop if it's not there
+  if(!msg.content.startsWith(prefix)) return;
+  // Exit if any bot
+  if(msg.author.bot) return;
+
+  else if (msg.content.startsWith(prefix + "flipcoin")) {
+    if (getRandomInteger(0, 1) == 1) {
+      msg.channel.sendMessage("Heads!");
+    }
+    else msg.channel.sendMessage("Tails!");
+  }
+  else if (msg.content.startsWith(prefix + "roll")) {
+    var text = msg.content;
+    msg.channel.sendMessage(roll(text));
+    }
+  else if (msg.content.startsWith(prefix + "xkcdrandom")) {
+    xkcdRandom(function (imgUrl) {
+      msg.channel.sendMessage(imgUrl);
+    });
+  }
+  else if (msg.content.startsWith(prefix + "xkcdlatest")) {
+    latestXkcd(function (imgUrl) {
+      msg.channel.sendMessage(imgUrl);
+    });
+  }
+});
+
+function xkcdRandom(callback) {
+  var min = 1;
+  getLatestXkcdNumber(function(response) {
+    var randomNumber = getRandomInteger(1, response);
+    if (randomNumber == 404) getRandomInteger(1, response);
+    var xkcdUrl = "http://xkcd.com/" + randomNumber + "/info.0.json";
+    getXkcdUrlAndTitle(xkcdUrl, (function(response) {
+      imgUrl = response;
+      callback(imgUrl);
+    })
+  )
+  });
+}
+
+function latestXkcd(callback) {
+    getLatestXkcdNumber(function(response) {
+    latest = response;
+    var xkcdUrl = "http://xkcd.com/" + latest + "/info.0.json";
+    getXkcdUrlAndTitle(xkcdUrl, (function(response) {
+      imgUrl = response;
+      callback(imgUrl);
+    }))
+});
+}
+
+function getLatestXkcdNumber(callback) {
+request('http://xkcd.com/info.0.json', (error, response, body) => {
+        latestComicNum = JSON.parse(body)['num']
+        callback(latestComicNum);
+      });
+    }
+
+function getXkcdUrlAndTitle(url, callback) {
+  var imgUrl;
+  var imgAlt;
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      xkcdBody = JSON.parse(body);
+      console.log(xkcdBody);
+      imgUrl = xkcdBody["img"];
+      console.log(imgUrl + " urli");
+      imgAlt = xkcdBody["alt"];
+      console.log(imgAlt + "altti");
+      var infos = [imgUrl, imgAlt];
+      callback();
+    }
+});
+}
+
+
+function roll(str) {
+  var rollResult;
+  var wrongInput = "Wrong input, type for example !roll 1 20";
+
+  var arrayText = str.split(" ");
+  if ("!roll" === str) {
+    rollResult = getRandomInteger(1, 10);
+    return rollResult;
+  }
+  else if (arrayText.length == 3) {
+    var min = Number(arrayText[1]);
+    var max = Number(arrayText[2]);
+
+      if (min < max && min != NaN && max != NaN) {
+      rollResult = getRandomInteger(max, min);
+      return rollResult;
+      }
+      else return wrongInput;
+    }
+    else return wrongInput;
+  }
+
+function isInteger(str) {
+    var n = Math.floor(Number(str));
+    return String(n) === str && n >= 0;
+}
+
+function createPoll(options) {
+  var pollOptions;
+}
+
+function getRandomInteger(min, max) {
+  var result = Math.floor(Math.random() * (max - min + 1)) + min;
+  return result;
+}
+
+
+function showError() {
+  msg.channel.sendMessage("Error!");
+}
+
+
+bot.login(INSERT YOUR KEY HERE);
